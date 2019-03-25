59787cfa6bf7424eb7785d78598bad54f2e297a0
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-03-25 19:45:44 +0200

    Add coverage to test command

--- test/lib/client.test.js
+++ test/lib/client.test.js
@@ -1,19 +1,19 @@
-const client = require("../../lib/client");
+const catalog = require("../../lib/catalog");
 const config = require("../../config");
 
 jest.setTimeout(10000);
 
 it("should return no more than 10 images when page size is 10", async () => {
-  const Client = new client(config.CLIENT_ID, config.CLIENT_SECRET);
+  const Catalog = new catalog(config.CLIENT_ID, config.CLIENT_SECRET);
   expect.assertions(1);
-  const res = await Client.imageSearch({ q: "test", pagesize: 10 });
+  const res = await Catalog.imageSearch({ q: "test", pagesize: 10 });
   expect(res.results.length).toBeLessThan(11);
 });
 
 it("should authorize when CLIENT_ID and CLIENT_SECRET are passed", async () => {
-  const Client = new client(config.CLIENT_ID, config.CLIENT_SECRET);
+  const Catalog = new catalog(config.CLIENT_ID, config.CLIENT_SECRET);
   expect.assertions(1);
-  const res = await Client.makeRequest({
+  const res = await Catalog.makeRequest({
     method: "get",
     url: "/image/search",
     params: {
