b14dd650ddbfa8ebcac0b7d2ae1ae5b3d0fcc74a
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-04-01 23:31:18 +0200

    Bundle with Webpack and add HTML example

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* global __dirname, require, module*/
+
+const env = require("yargs").argv.env; // use --env with webpack 2
+const pkg = require("./package.json");
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === "build") {
+  mode = "production";
+  outputFile = libraryName + ".min.js";
+} else {
+  mode = "development";
+  outputFile = libraryName + ".js";
+}
+
+const config = {
+  mode, // Sets the value of mode to build OR development
+  entry: __dirname + "/lib/catalog.js",
+  devtool: "inline-source-map",
+  output: {
+    path: __dirname + "/dist",
+    filename: outputFile,
+    library: "catalog",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  }
+};
+
+module.exports = config;
